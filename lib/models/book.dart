421462639d1a75a5ff974f5711d04c674e66334e
Katie <25576005+stuttskl@users.noreply.github.com>
2021-03-11 10:12:08 -0800

    added basic Book Model

---
+++ lib/models/book.dart
@@ -0,0 +1,17 @@
+class Book {
+  final String title;
+  final String author;
+  final double rating;
+  final String startDate;
+  final String endDate;
+  final bool current;
+
+  Book({
+    this.title,
+    this.author,
+    this.rating,
+    this.startDate,
+    this.endDate,
+    this.current
+  });
+}
