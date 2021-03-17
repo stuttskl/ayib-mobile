b99116cfc2b132029e274bde22b285217395cf15
Katie <25576005+stuttskl@users.noreply.github.com>
2021-03-17 11:23:37 -0700

    added reading progress bar w hardcoded percentage

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-
+import 'package:syncfusion_flutter_charts/charts.dart';
 import '../widgets/current_book.dart';
 import '../widgets/add_form.dart';
 
@@ -9,21 +9,15 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-    int _selectedIndex = 0;
 
-
-  void _onItemTapped(int index) {
-    setState(() {
-      _selectedIndex = index;
-    });
-  }
   @override
+  
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.center,
-          // mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: [
             RichText(
               text: TextSpan(
@@ -31,35 +25,20 @@ class _HomeScreenState extends State<HomeScreen> {
                 style: Theme.of(context).textTheme.headline4,
               )
             ),
-            // AddForm(),
             CurrentBook(
               author: 'Cixin Liu',
               title: 'The Three Body Problem',
               imgUrl: 'https://images-na.ssl-images-amazon.com/images/I/919XM42JQlL.jpg'
             ),
-
           ]
         ),
       ),
-      // bottomNavigationBar: BottomNavigationBar(
-      //   items: const <BottomNavigationBarItem>[
-      //     BottomNavigationBarItem(
-      //       icon: Icon(Icons.home),
-      //       label: 'Home',
-      //     ),
-      //     BottomNavigationBarItem(
-      //       icon: Icon(Icons.business),
-      //       label: 'Business',
-      //     ),
-      //     BottomNavigationBarItem(
-      //       icon: Icon(Icons.school),
-      //       label: 'School',
-      //     ),
-      //   ],
-      //   currentIndex: _selectedIndex,
-      //   selectedItemColor: Colors.amber[800],
-      //   onTap: _onItemTapped,
-      // ),
     );
   }
+}
+class ChartData {
+    ChartData(this.x, this.y, [this.color]);
+    final String x;
+    final double y;
+    final Color color;
 }
